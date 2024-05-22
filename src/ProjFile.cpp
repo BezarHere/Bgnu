b31e4e8ca2e4e6d529feb1f1a841ee2a050e39de
botatodev <botatodev@gmail.com>
2024-05-22 22:31:46 +0300

    project file stuff

--- src/ProjFile.cpp
+++ src/ProjFile.cpp
@@ -1,4 +1,5 @@
 #include "ProjFile.hpp"
+#include "Console.hpp"
 #include <fstream>
 #include <string.h>
 
@@ -231,9 +232,11 @@ inline Token Tokenizer::_read_tk() const {
 class Parser
 {
 public:
-	inline Parser(const char *source, const size_t source_length,
-								const Token *tks, const size_t count)
-		: m_source{source}, m_source_length{source_length}, m_tokens{tks}, m_size{count} {
+	static constexpr char ValueAssignOp = ':';
+	static constexpr char ValueSeparateOp = ',';
+
+	inline Parser(const Token *tks, const size_t count)
+		: m_tokens{tks}, m_size{count} {
 	}
 
 	ProjVar::VarDict parse();
@@ -247,104 +250,263 @@ public:
 		return token.type == TokenType::Whitespace || token.type == TokenType::Unknown;
 	}
 
-	inline const Token *get_ptr() const noexcept { return &m_tokens[current_index]; }
-	inline const Token &get_current() const noexcept { return m_tokens[current_index]; }
+	static bool is_empty_token(const Token &token) {
+		return is_useless_token(token) || token.type == TokenType::Newline;
+	}
+
+	// current ptr
+	inline const Token *current() const noexcept { return &m_tokens[current_index]; }
+	inline const Token &get_tk() const noexcept { return m_tokens[current_index]; }
 
 	inline size_t size() const noexcept { return m_size; }
 	inline const Token *data() const noexcept { return m_tokens; }
 
 	size_t current_index;
+
 private:
-	void _skip_useless();
+	ALWAYS_INLINE void _advance_tk() {
+		++current_index;
+	}
 
-	ProjVar _get_var();
-	std::pair<ProjVar::VarString, ProjVar> _get_kv();
+	ALWAYS_INLINE void _advance_tk(size_t amount) {
+		if (amount == 0)
+		{
+			//! ERROR?
+		}
+
+		current_index += amount;
+	}
+
+	ALWAYS_INLINE bool _can_read() const {
+		return current_index < m_size - 1;
+	}
+
+	inline void _skip_useless() {
+		while (current_index < m_size && Parser::is_useless_token(m_tokens[current_index]))
+		{
+			++current_index;
+		}
+	}
+
+	inline void _skip_empty() {
+		while (current_index < m_size && Parser::is_empty_token(m_tokens[current_index]))
+		{
+			++current_index;
+		}
+	}
+
+	ProjVar _parse_var();
+	std::pair<ProjVar::VarString, ProjVar> _parse_var_kv();
 
 
-	ProjVar _parse_var_string(bool trim);
-	ProjVar _parse_var_identifier();
-	ProjVar _parse_var_number();
+	ProjVar::VarString _parse_var_string() const;
+	ProjVar::VarNumber _parse_var_number() const;
+	ProjVar::VarArray _parse_var_array();
+	ProjVar::VarDict _parse_var_dict(const bool body_dict = false);
 
+	ProjVar _parse_var_identifier() const;
 	ProjVar _parse_var_simple();
-	ProjVar _parse_var_dict();
-	ProjVar _parse_var_array();
 
+	NORETURN void _XUnexpectedToken(const string &expected = "") const {
+		char buffer[inner::ExceptionBufferSz]{};
+
+		if (!expected.empty())
+		{
+			sprintf_s(
+				buffer, "Unexpected token at %u:%u, expected '%s'",
+				get_tk().pos.line, get_tk().pos.column, expected.c_str()
+			);
+		}
+		else
+		{
+			sprintf_s(buffer, "Unexpected token at %u:%u", get_tk().pos.line, get_tk().pos.column);
+		}
+
+		throw std::runtime_error(buffer);
+	}
 
 private:
-	const char *m_source;
-	const size_t m_source_length;
 	const Token *m_tokens;
 	const size_t m_size;
 };
 
-void Parser::_skip_useless() {
-	while (current_index < m_size && Parser::is_useless_token(m_tokens[current_index]))
+ProjVar::VarString Parser::_parse_var_string() const {
+	if (get_tk().type != TokenType::String)
 	{
-		++current_index;
+		return {get_tk().str, get_tk().length};
 	}
+
+	if (get_tk().length < 2)
+	{
+		return {};
+	}
+
+	return {get_tk().str + 1, get_tk().length - 2};
 }
 
-
-ProjVar Parser::_parse_var_string(bool trim) {
-	return ProjVar();
-}
-
-ProjVar Parser::_parse_var_identifier() {
-	const char *token_str = m_source + get_current().start;
+ProjVar Parser::_parse_var_identifier() const {
+	const char *token_str = get_tk().str;
 	constexpr const char null_str[] = "null";
 	constexpr const char true_str[] = "true";
 	constexpr const char false_str[] = "false";
 
 	if (strncmp(token_str, null_str, std::size(null_str) - 1) == 0)
+	{
 		return ProjVar(ProjVarType::Null);
+	}
 
 	if (strncmp(token_str, true_str, std::size(true_str) - 1) == 0)
-		return ProjVar(true);
+	{
+		return true;
+	}
 
 	if (strncmp(token_str, false_str, std::size(false_str) - 1) == 0)
-		return ProjVar(false);
+	{
+		return false;
+	}
 
-	return ProjVar::VarString(token_str, get_current().length);
+	return ProjVar::VarString(token_str, get_tk().length);
 }
 
-ProjVar Parser::_parse_var_number() {
+ProjVar::VarNumber Parser::_parse_var_number() const {
 	// TODO: error detection
 
-	float val = strtof(m_source + get_current().start, nullptr);
+	float val = strtof(get_tk().str, nullptr);
 
-	return ProjVar(val);
+	return val;
 }
 
 ProjVar Parser::_parse_var_simple() {
-	if (get_current().type == TokenType::String)
-		return _parse_var_string(true);
+	_skip_useless();
 
-	if (isdigit(m_source[get_current().start]) == 0)
+	if (get_tk().type == TokenType::String)
+		return _parse_var_string();
+
+	if (is_number_identifier(get_tk().str, get_tk().length))
 		return _parse_var_number();
 
 	return _parse_var_identifier();
 }
 
-ProjVar Parser::_parse_var_dict() {
-	return ProjVar();
-}
-
-ProjVar Parser::_parse_var_array() {
-	return ProjVar();
-}
-
-ProjVar Parser::_get_var() {
+ProjVar::VarDict Parser::_parse_var_dict(const bool body_dict) {
 	_skip_useless();
 
-	switch (get_current().type)
+	// skip starting '{' for non-body dicts
+	if (!body_dict && get_tk().type == TokenType::Open_CurlyBracket)
+	{
+		_advance_tk();
+	}
+
+	ProjVar::VarDict dict{};
+
+	while (_can_read())
+	{
+		_skip_empty();
+
+		// parse next tokens as value and added to the constructed array
+		dict.emplace(_parse_var_kv());
+
+		_skip_empty();
+		const bool has_leading_comma = get_tk().type == TokenType::Comma;
+
+		// did we hit a comma after value? then skip it
+		if (has_leading_comma)
+		{
+			_advance_tk();
+			_skip_empty();
+		}
+
+		if (body_dict)
+		{
+			// for body dicts, we close on EOF
+			if (get_tk().type == TokenType::Eof)
+			{
+				break;
+			}
+
+		}
+		else
+		{
+
+			// did we hit the closing bracket? skip it (to mark it read) and break
+			// if we didn't hit a closing bracket, then there *must be a next value*
+			if (get_tk().type == TokenType::Close_CurlyBracket)
+			{
+				_advance_tk();
+				break;
+			}
+		}
+
+		// the list has a next value with no comma separating them
+		// if it didn't have a next value, then it would have had stoped above
+		if (!has_leading_comma)
+		{
+			_XUnexpectedToken(format_join('\'', ValueSeparateOp, "' or '}'"));
+		}
+
+	}
+
+	return dict;
+}
+
+ProjVar::VarArray Parser::_parse_var_array() {
+	_skip_useless();
+
+	if (get_tk().type == TokenType::Open_SqBracket)
+	{
+		_advance_tk();
+	}
+
+	ProjVar::VarArray array{};
+	while (_can_read())
+	{
+		_skip_empty();
+
+		// parse next tokens as value and added to the constructed array
+		array.emplace_back(_parse_var());
+
+		_skip_empty();
+		const bool has_leading_comma = get_tk().type == TokenType::Comma;
+
+		// did we hit a comma after value? then skip it
+		if (has_leading_comma)
+		{
+			_advance_tk();
+			_skip_empty();
+		}
+
+		// did we hit the closing bracket? skip it (to mark it read) and break
+		// if we didn't hit a closing bracket, then there must be a next value
+		if (get_tk().type == TokenType::Close_SqBracket)
+		{
+			_advance_tk();
+			break;
+		}
+
+		// the list has a next value with no comma separating them
+		// if it didn't have a next value, then it would have had stoped above
+		if (!has_leading_comma)
+		{
+			_XUnexpectedToken(format_join('\'', ValueSeparateOp, "' or ']'"));
+		}
+
+	}
+
+	return array;
+}
+
+ProjVar Parser::_parse_var() {
+	_skip_useless();
+
+	switch (get_tk().type)
 	{
 	case TokenType::Identifier:
 	case TokenType::String:
 		return _parse_var_simple();
 	case TokenType::Open_CurlyBracket:
-		return _parse_var_dict();
+		return ProjVar(_parse_var_dict());
 	case TokenType::Open_SqBracket:
-		return _parse_var_array();
+		return ProjVar(_parse_var_array());
 
 	default:
 		//! Report the error with more info
@@ -354,16 +516,26 @@ ProjVar Parser::_get_var() {
 	return ProjVar(0.0F);
 }
 
-std::pair<ProjVar::VarString, ProjVar> Parser::_get_kv() {
+std::pair<ProjVar::VarString, ProjVar> Parser::_parse_var_kv() {
+
 	_skip_useless();
+	ProjVar::VarString key = _parse_var_string();
 
+	_skip_empty();
 
+	if (get_tk().type != TokenType::Column)
+	{
+		_XUnexpectedToken(string(1, ValueAssignOp));
+	}
 
-	return std::pair<ProjVar::VarString, ProjVar>();
+	_advance_tk();
+	_skip_empty();
+
+	return {key, _parse_var()};
 }
 
 ProjVar::VarDict Parser::parse() {
-	return ProjVar::VarDict();
+	return _parse_var_dict(true);
 }
 
 #pragma endregion
@@ -390,7 +562,7 @@ ProjVar ProjFile::read(const char *source, size_t length) {
 
 	tokenizer.put_all(tokens);
 
-	Parser parser{source, length, tokens.data(), tokens.size()};
+	Parser parser{tokens.data(), tokens.size()};
 
 	return ProjVar(parser.parse());
 }
