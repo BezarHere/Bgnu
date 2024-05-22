bf095d78de770ba06639513ca675e708770365a0
botatodev <botatodev@gmail.com>
2024-05-22 22:31:35 +0300

    utility macros to base.hpp

--- src/base.hpp
+++ src/base.hpp
@@ -10,10 +10,26 @@ using std::string;
 using std::vector;
 using std::array;
 
+
 static constexpr size_t npos = (size_t)-1;
+
+
+#ifdef _MSC_VER
+#define NODISCARD _NODISCARD
+#define NORETURN __declspec(noreturn)
+#define ALWAYS_INLINE inline __forceinline
+#elif defined(__GNUC__) || defined(__clang__)
+#define NODISCARD [[__nodiscard__]]
+#define NORETURN __attribute__ ((__noreturn__))
+#define ALWAYS_INLINE inline __attribute__((__always_inline__))
+#endif
+
+
 
 namespace inner
 {
+	static constexpr size_t ExceptionBufferSz = 1024;
+
 	struct EmptyCTor
 	{
 		template <class T>
