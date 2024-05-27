38fa79a6187f0f08b33c316cd8ceb4d671e3acc0
botatodev <botatodev@gmail.com>
2024-05-27 04:22:32 +0300

    renamed the TRange::start to be TRange::begin

--- src/Range.hpp
+++ src/Range.hpp
@@ -1,32 +1,32 @@
 #pragma once
 #include "base.hpp"
 
-
-/// @brief inclusive start, exclusive end range
+/// @brief inclusive begin, exclusive end range
 /// @tparam _T the range value type
 template <typename _T>
 struct TRange
 {
 	static_assert(std::is_arithmetic_v<_T> || std::is_pointer_v<_T>, "only arithmetic or pointer ranges are allowed");
+	using iterable = TRangeIterable<_T>;
 
 	inline constexpr TRange() = default;
 
-	inline constexpr TRange(_T pstart, _T pend) : start{pstart}, end{pend} {
+	inline constexpr TRange(_T pstart, _T pend) : begin{pstart}, end{pend} {
 	}
 
 	inline constexpr _T length() const {
-		return end - start;
+		return end - begin;
 	}
 
 	inline constexpr bool valid() const {
-		return end > start;
+		return end > begin;
 	}
 
 	inline constexpr bool contains(const _T value) const {
-		return value < end && value >= start;
+		return value < end && value >= begin;
 	}
 
-	_T start{};
+	_T begin{};
 	_T end{};
 };
 
