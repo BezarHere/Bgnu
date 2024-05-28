a17b70e1aac15f11539bc8d453a4aa69bf659e92
botatodev <botatodev@gmail.com>
2024-05-28 06:07:54 +0300

    more glob work

--- src/Glob.cpp
+++ src/Glob.cpp
@@ -22,11 +22,11 @@ struct Glob::Segment
 {
 	struct DataCharacters
 	{
-		size_t count;
+		size_t count = 0;
 		std::array<string::value_type, MaxSegmentCharacters> chars;
 	};
 
-	inline Segment(SegmentType _type, const IndexRange &_range, const DataCharacters &_data_chars)
+	inline Segment(SegmentType _type, const IndexRange &_range, const DataCharacters &_data_chars = {})
 		: type{_type}, range{_range}, data_chars{_data_chars} {
 
 	}
@@ -37,18 +37,89 @@ struct Glob::Segment
 	DataCharacters data_chars;
 };
 
+static Glob::Segment parse_char_selector(const StrBlob &blob) {
+	size_t closure_counter = 0;
+	size_t end = blob.size;
+
+	for (size_t i = 0; i < blob.size; i++)
+	{
+		if (blob[i] == '[')
+		{
+			closure_counter++;
+			continue;
+		}
+
+		if (blob[i] == ']')
+		{
+			if (closure_counter == 0) {
+				end = i;
+				break;
+			}
+
+			closure_counter--;
+			continue;
+		}
+	}
+
+	if (end == blob.size)
+	{
+		//? should this print out an error?
+	}
+
+	// after '[' and before ']'
+	IndexRange chars_range{1, end};
+
+	
+
+}
 
 Glob::Glob(const string &str) : m_source{str}, m_segments{parse(str)} {
 }
 
 Glob::SegmentCollection Glob::parse(const StrBlob &blob) {
-	string ss = "3123";
+	vector<Segment> segments{};
+
 	for (size_t i = 0; i < blob.size; i++)
 	{
 		if (FilePath::is_directory_separator(blob[i]))
 		{
 			size_t count = StringUtils::count(&blob[i], blob.size - i, FilePath::is_directory_separator);
+			i += count - 1;
+			segments.emplace_back(SegmentType::DirectorySeparator, IndexRange(i, i + count));
+			continue;
 		}
+
+		if (blob[i] == '?')
+		{
+			size_t count = StringUtils::count(&blob[i], blob.size - i, inner::EqualTo('?'));
+			i += count - 1;
+			segments.emplace_back(SegmentType::AnyCharacter, IndexRange(i, i + count));
+			continue;
+		}
+
+		if (blob[i] == '*')
+		{
+			size_t count = StringUtils::count(
+				&blob[i],
+				// check either 1 or two stars in a row
+				std::min<size_t>(blob.size - i, 2),
+				inner::EqualTo('*')
+			);
+
+			i += count - 1;
+
+			segments.emplace_back(
+				count == 2 ? SegmentType::AnyPath : SegmentType::AnyName,
+				IndexRange(i, i + count)
+			);
+			continue;
+		}
+
+		if (blob[i] == '[')
+		{
+			segments.push_back(parse_char_selector(blob.slice(i)));
+		}
+
 	}
 
 	return nullptr;
