68ff156d315d659fda7e6ac6e91f604b114080e0
botatodev <botatodev@gmail.com>
2024-05-31 19:20:32 +0300

    made main call startup::start

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,7 @@
-#include "FieldFile.hpp"
-#include "Project.hpp"
-#include "Command.hpp"
+#include "Startup.hpp"
 
 #include <iostream>
 
 int main(int argc, const char *argv[]) {
-	CommandDB::_load_commands();
-	ArgumentReader reader = ArgumentReader::from_args(argv, argc);
-
-	std::cout << "hello\n";
-
-	std::cout << std::boolalpha;
-
-	Glob glob = "**\\*.[ch][px][px]";
-	
-	string path;
-	std::cin >> path;
-
-	std::cout << "\n\"" << path << "\" matches as " << glob.test(path) << '\n';
-
-	FieldVar loaded_file = FieldFile::load("F:\\gcc\\bgnu\\test.txt");
-	ErrorReport report{};
-	Project project = Project::from_data(loaded_file.get_dict(), report);
-
-	std::cin.get();
+	Startup::start(ArgumentReader::from_args(argv, argc));
 }
