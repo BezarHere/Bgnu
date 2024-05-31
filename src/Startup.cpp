ea05dfda05e08327815172418351a6d10c6f2d0f
botatodev <botatodev@gmail.com>
2024-05-31 22:34:23 +0300

    startup now checks for commands and suggest commands with spellchecking

--- src/Startup.cpp
+++ src/Startup.cpp
@@ -1,7 +1,58 @@
 #include "Startup.hpp"
+#include "Logger.hpp"
+#include "Command.hpp"
 
-
+FieldVar::Dict Startup::s_env = {};
 
 void Startup::start(ArgumentReader reader) {
-	
+	if (reader.count() == 0)
+	{
+		Logger::warning("no command passed, please pass a command and it's arguments");
+		return;
+	}
+
+
+	_build_env();
+	CommandDB::_load_commands();
+
+	const Argument *command_arg = reader.get_next();
+
+	Command *command = CommandDB::get_command(command_arg->value);
+
+	if (command == nullptr)
+	{
+		Logger::error("No command named '%s'", to_cstr(command_arg->value));
+		_check_misspelled_command(command_arg->value);
+		return;
+	}
+
+}
+
+void Startup::_build_env() {
+	s_env.emplace("min_broadcast_conf", 0.65F);
+}
+
+void Startup::_check_misspelled_command(const string &name) {
+	volatile auto suggestion = CommandDB::get_suggestion(name);
+
+
+
+	if (suggestion.command == nullptr)
+	{
+		Logger::debug("Suggestion: no command matched even slightly");
+		return;
+	}
+
+	Logger::debug(
+		"Suggestion: command_name=\"%s\", confidence=%0.2f",
+		to_cstr(suggestion.command->name),
+		suggestion.confidence
+	);
+
+	if (suggestion.confidence < (float)s_env.at("min_broadcast_conf"))
+	{
+		return;
+	}
+
+	Logger::note("Maybe you meant '%s'?", to_cstr(suggestion.command->name));
 }
--- src/Startup.hpp
+++ src/Startup.hpp
@@ -1,8 +1,17 @@
 #pragma once
 #include "Argument.hpp"
+#include "FieldVar.hpp"
 
 class Startup
 {
 public:
 	static void start(ArgumentReader reader);
+
+private:
+	static void _build_env();
+	static void _check_misspelled_command(const string &name);
+
+
+private:
+	static FieldVar::Dict s_env;
 };
