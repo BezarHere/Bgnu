a651c940f78a570162ae83cc919f6124fc9ae778
botatodev <botatodev@gmail.com>
2024-05-31 22:35:02 +0300

    fixed passing all args (first one is the exc path) + args debug printout

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "Startup.hpp"
 
 #include <iostream>
+#include "Logger.hpp"
 
 int main(int argc, const char *argv[]) {
-	Startup::start(ArgumentReader::from_args(argv, argc));
+	for (int i = 0; i < argc; i++)
+	{
+		Logger::debug("arg no.%d: '%s'", i, argv[i]);
+	}
+
+	Startup::start(ArgumentReader::from_args(argv + 1, std::max(argc - 1, 0)));
 }
