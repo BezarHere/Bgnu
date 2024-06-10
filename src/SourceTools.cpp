aef48959169ea3b95ee01eefe234370435bd94d6
botatodev <botatodev@gmail.com>
2024-06-10 22:37:49 +0300

    source tools changes

--- src/SourceTools.cpp
+++ src/SourceTools.cpp
@@ -1,205 +1,34 @@
 #include "SourceTools.hpp"
 #include "FilePath.hpp"
+#include "code/CPreprocessor.hpp"
 
-#include <regex>
 
-struct CPreprocessor
-{
 
-	enum class Type : uint8_t
+vector<string> SourceTools::get_dependencies(const StrBlob &file, SourceType type) {
+	switch (type)
 	{
-		None,
-		Unknown,
-
-		Include = 4,
-
-		Define,
-		Undefine,
-
-		If,
-		Else,
-		Elif,
-		EndIf,
-
-		IfDefined,
-		IfNotDefined,
-
-		Error,
-		Warning,
-
-		Pragma,
-	};
-
-	struct Token
-	{
-		inline Token() = default;
-		Token(const IndexRange &name, const IndexRange &value);
-
-		Type type = Type::None;
-		IndexRange name;
-		IndexRange value; // or anything right of the statement
-		uint32_t line = 0;
-	};
-
-	static Token get_next_tk(const StrBlob &source);
-
-	static Token _read_tk_inline(const StrBlob &source);
-	static Token _parse_tk(const StrBlob &line);
-
-	static size_t _escape(const StrBlob &source, const MutableStrBlob &destination);
-
-};
-
-vector<string> SourceTools::get_dependencies(const StrBlob &file) {
-	return vector<string>();
-}
-
-CPreprocessor::Token CPreprocessor::get_next_tk(const StrBlob &source) {
-
-	for (size_t i = 0; i < source.size - 1; i++)
-	{
-		if (StringTools::is_newline(source[i]) && !StringTools::is_newline(source[i + 1]))
+	case SourceType::C:
+		// case SourceType::CPP: // equal to SourceType::C
 		{
-			return _read_tk_inline(source.slice(i + 1));
-		}
-	}
+			vector<CPreprocessor::Token> tokens{};
 
-	return Token();
-}
+			CPreprocessor::gather_all_tks(file, tokens);
 
-CPreprocessor::Token CPreprocessor::_read_tk_inline(const StrBlob &source) {
-	size_t hashtag_pos = npos;
+			vector<string> results{};
 
-	size_t index = 0;
-	for (; index < source.size; index++)
-	{
-		if (StringTools::is_newline(source[index]))
-		{
-			break;
-		}
-
-		if (source[index] == '\\')
-		{
-			index++;
-
-			for (; index < source.size; index++)
+			for (const auto &token : tokens)
 			{
-				if (StringTools::is_newline(source[index]))
+				if (token.type == CPreprocessor::Type::Include)
 				{
-					break;
+					results.push_back(
+						CPreprocessor::get_include_path(file.slice(token.value.begin, token.value.end))
+					);
 				}
-
-				if (StringTools::is_whitespace(source[index]))
-				{
-					continue;
-				}
-
-				break;
-			}
-		}
-
-		if (hashtag_pos == npos && source[index] == '#')
-		{
-			hashtag_pos = index;
-		}
-
-	}
-
-	if (hashtag_pos == npos)
-	{
-		return Token();
-	}
-	const StrBlob line = source.slice(hashtag_pos + 1, index);
-	string_char *escaped = new string_char[line.size + 1]{0};
-
-	size_t length = _escape(line, {escaped, line.size});
-
-	Token token = _parse_tk({escaped, length});
-
-	delete[] escaped;
-
-	return token;
-}
-
-CPreprocessor::Token CPreprocessor::_parse_tk(const StrBlob &line) {
-	constexpr auto _is_valid_macro_name = \
-		[](string_char value) { return value == '_' || isalnum(value); };
-
-	IndexRange name = {};
-	IndexRange value = {};
-
-	for (size_t i = 0; i < line.size; i++)
-	{
-		if (!name.valid())
-		{
-			if (_is_valid_macro_name(line[i]))
-			{
-				size_t count = StringTools::count(
-					line.data + i,
-					line.size - i,
-					_is_valid_macro_name
-				);
-
-				name = {i, i + count};
-
-				i += count;
-				i--;
 			}
 
-			continue;
-		}
-
-		if (!StringTools::is_whitespace(line[i]))
-		{
-			size_t trailing_ws = StringTools::rcount(
-				line.data + i,
-				line.size - i,
-				StringTools::is_whitespace
-			);
-
-			value = {i, line.size - trailing_ws};
+			return
 		}
 	}
-
-	return Token(name, value);
+	return;
 }
 
-size_t CPreprocessor::_escape(const StrBlob &source, const MutableStrBlob &destination) {
-	size_t offset = 0;
-	for (size_t i = 0; i < source.size; i++)
-	{
-		if (source[i] == '\n' && i < source.size - 1)
-		{
-			// next char + advance offset
-			i++;
-			offset += 2;
-
-			// skip escaped char
-			if (StringTools::is_whitespace(source[i]))
-			{
-				continue;
-			}
-
-			// revert back (only escaped whitespace in macros)
-			i--;
-			offset -= 2;
-		}
-
-		if (i - offset >= destination.size)
-		{
-			break;
-		}
-
-		destination[i - offset] = source[i];
-	}
-
-	destination[source.size - offset] = 0;
-
-	return source.size - offset;
-}
-
-CPreprocessor::Token::Token(const IndexRange &_name, const IndexRange &_value)
-	: name{_name}, value{_value} {
-	constexpr pair<const char *, Type> Names[]
-	
-}
--- src/SourceTools.hpp
+++ src/SourceTools.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include "base.hpp"
 
+enum class SourceType
+{
+	C,
+	CPP = C,
+
+};
+
 class SourceTools
 {
-	static vector<string> get_dependencies(const StrBlob &file);
+	static vector<string> get_dependencies(const StrBlob &file, SourceType type);
 };
 
