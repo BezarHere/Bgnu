93e1ac9cf060c2304654ac509aaf986a223350dd
botatodev <botatodev@gmail.com>
2024-06-15 15:11:41 +0300

    source tools changes

--- src/SourceTools.cpp
+++ src/SourceTools.cpp
@@ -4,34 +4,68 @@
 
 
 
-vector<string> SourceTools::get_dependencies(const StrBlob &file, SourceType type) {
+void SourceTools::get_dependencies(const StrBlob &file, SourceFileType type,
+																	 vector<string> &out) {
 	switch (type)
 	{
-	case SourceType::C:
+	case SourceFileType::C:
 		// case SourceType::CPP: // equal to SourceType::C
 		{
 			vector<CPreprocessor::Token> tokens{};
 
 			CPreprocessor::gather_all_tks(file, tokens);
 
-			vector<string> results{};
 
 			for (const auto &token : tokens)
 			{
 				if (token.type == CPreprocessor::Type::Include)
 				{
-					results.push_back(
+					out.push_back(
 						CPreprocessor::get_include_path(file.slice(token.value.begin, token.value.end))
 					);
 				}
 			}
 
-			return results;
+			return;
 		}
 
 	default:
 		Logger::error("get_dependencies() of type %d is not implemented", (int)type);
-		return {};
+		return;
 	}
 }
 
+void SourceProcessor::add_file(const FilePath &filepath) {
+	auto data = filepath.read_string();
+	return add_file(StrBlob(data.data(), data.length()));
+}
+
+void SourceProcessor::add_file(const StrBlob &source) {
+
+
+
+}
+
+void SourceProcessor::process() {
+}
+
+hash_t SourceProcessor::get_file_hash(const FilePath &filepath) const {
+	return m_info_map.at(filepath).hash;
+}
+
+bool SourceProcessor::is_processing_path(const FilePath &filepath) const {
+	for (const auto &fp : m_in_loading)
+	{
+		if (fp == filepath)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void SourceProcessor::process_dependencies(const FilePath &filepath) {
+	const DependencyInfo &dep_info = m_info_map.at(filepath);
+
+
+}
--- src/SourceTools.hpp
+++ src/SourceTools.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include "base.hpp"
+#include "FilePath.hpp"
 
-enum class SourceType
+enum class SourceFileType
 {
 	C,
 	CPP = C,
@@ -11,6 +12,35 @@ enum class SourceType
 class SourceTools
 {
 public:
-	static vector<string> get_dependencies(const StrBlob &file, SourceType type);
+	static void get_dependencies(const StrBlob &file, SourceFileType type, vector<string> &out);
 };
 
+class SourceProcessor
+{
+public:
+	struct DependencyInfo
+	{
+		hash_t hash = 0;
+		vector<FilePath> sub_dependencies;
+		vector<FilePath> included_directories;
+	};
+
+	typedef std::map<string, vector<string>> dependency_map;
+	typedef std::map<FilePath, DependencyInfo> dependency_info_map;
+
+	void add_file(const FilePath &filepath);
+	void add_file(const StrBlob &source);
+
+	void process();
+
+	hash_t get_file_hash(const FilePath &filepath) const;
+
+private:
+	bool is_processing_path(const FilePath &filepath) const;
+	void process_dependencies(const FilePath &filepath);
+
+private:
+	SourceFileType m_type;
+	dependency_info_map m_info_map;
+	vector<FilePath> m_in_loading;
+};
