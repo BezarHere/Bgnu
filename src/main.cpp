6d9c5d78239dbe8d48f8bc8964c87984e2ee8eac
botatodev <botatodev@gmail.com>
2025-01-19 00:37:19 +0300

    removed the buggy archiver test

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,6 @@ namespace chrono
 int main(int argc, const char *argv[]) {
 	const chrono::time_point pre_run_time = chrono::steady_clock::now();
 
-  auto x = Archiver_FileOpenPath("test.a", true);
-
-  archive_t container;
-
-  Archiver_ReadIO(&x, &container);
-
-  for (uint32_t i = 0; i < container.files_count; i++)
-  {
-    printf("file: '%s'\n", container.files[i].file_name);
-  }
-
-  Archiver_Close(&container);
-
-  Archiver_FileClose(&x);
-
 	int result = Startup::start(ArgumentReader(argv + 1, std::max(argc - 1, 0)));
 
 	const auto run_time = chrono::duration_cast<chrono::milliseconds>(
