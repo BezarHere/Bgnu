a18738cc7da02f601fa3c4148fc4dc9eb234fdce
botatodev <botatodev@gmail.com>
2025-05-04 03:46:52 +0300

    refactored the C-Preprocessor interface, made it more streamlined

--- src/code/CPreprocessor.cpp
+++ src/code/CPreprocessor.cpp
@@ -2,239 +2,276 @@
 #include "CodeTokenizer.hpp"
 
 
+constexpr auto whitespace_no_newlines = [](char cur) {
+  return string_tools::is_whitespace(cur) && !string_tools::is_newline(cur);
+  };
+
 constexpr CPreprocessor::Type CPreprocessor::_get_tk_type(const StrBlob &name) {
-	for (const auto &p : TypeNamePairs)
-	{
-		if (string_tools::equal(name.data, p.first, name.length()))
-		{
-			return p.second;
-		}
-	}
+  for (const auto &p : TypeNamePairs)
+  {
+    if (string_tools::equal(name.data, p.first, name.length()))
+    {
+      return p.second;
+    }
+  }
 
-	return Type::Unknown;
+  return Type::Unknown;
 }
 
-void CPreprocessor::gather_all_tks(const StrBlob &source, vector<Token> &output) {
-	size_t index = 0;
-	while (index < source.size)
-	{
-		Token token = get_next_tk(source.slice(index));
+void CPreprocessor::gather_all_tks(const StrBlob &input, vector<Token> &output) {
+  string processed = _escape(input);
 
-		size_t end = std::max(token.name.end, token.value.end);
+  CharSource source = {processed};
 
-		// std::cout << "token n1: \"" << string(token.name.begin + source.data, token.name.length()) << '"' << '\n';
-		// std::cout << "token v1: \"" << string(token.value.begin + source.data, token.value.length()) << '"' << '\n';
+  while (!source.depleted())
+  {
+    Token token = get_next_tk(source);
 
-		// offset token to the entire source (NOTE THAT: end is calculated before shifting the token)
-		token.name = token.name.shifted(index);
-		token.value = token.value.shifted(index);
+    if (token.type != Type::None)
+    {
+      output.push_back(token);
+    }
 
-		if (token.type == Type::None)
-		{
-			break;
-		}
+    if (source.depleted())
+    {
+      break;
+    }
 
-		index += end + 1;
-
-		output.push_back(token);
-	}
-}
-
-CPreprocessor::Token CPreprocessor::get_next_tk(const StrBlob &source) {
-
-	for (size_t i = 0; i < source.size - 1; i++)
-	{
-		if ((string_tools::is_newline(source[i]) || i == 0) && !string_tools::is_newline(source[i + 1]))
-		{
-
-			const size_t read_start = i + (i ? 1 : 0);
-
-			// + 1 to i if it's not zero
-			Token token = _read_tk_inline(source.slice(read_start));
-			token.name = token.name.shifted(read_start);
-			token.value = token.value.shifted(read_start);
-
-			/*
-			std::cout << "TOK PARSED N: " << token.name
-				<< " \"" << string(source.data + token.name.begin, token.name.length()) << "\"\n";
-
-			std::cout << "TOK PARSED V: " << token.value
-				<< " \"" << string(source.data + token.value.begin, token.value.length()) << "\"\n";
-			*/
-			return token;
-		}
-	}
-
-	return Token();
-}
-
-string CPreprocessor::get_include_path(const StrBlob &value) {
-	constexpr auto is_opening = [](string_char value) {return value == '"' || value == '<';};
-	constexpr auto is_closing = [](string_char value) {return value == '"' || value == '>';};
-
-	// pos of the opening quote/bracket
-	size_t opening = CodeTokenizer::match_unescaped(
-		value,
-		is_opening
-	);
-
-	if (opening == npos)
-	{
-		return "";
-	}
-
-	opening = opening + 1;
-
-	// pos of the closing quote/bracket
-	const size_t closing = CodeTokenizer::match_unescaped(
-		value.slice(opening),
-		is_closing
-	) + opening;
-
-	return string(value.data + opening, closing - opening);
-}
-
-CPreprocessor::Token CPreprocessor::_read_tk_inline(const StrBlob &source) {
-	size_t hashtag_pos = npos;
-
-	size_t index = 0;
-	for (; index < source.size; index++)
-	{
-		if (string_tools::is_newline(source[index]))
-		{
-			break;
-		}
-
-		if (source[index] == '\\')
-		{
-			index++;
-
-			for (; index < source.size; index++)
-			{
-				if (string_tools::is_newline(source[index]))
-				{
-					break;
-				}
-
-				if (string_tools::is_whitespace(source[index]))
-				{
-					continue;
-				}
-
-				break;
-			}
-		}
-
-
-		if (hashtag_pos == npos && source[index] == '#')
-		{
-
-			hashtag_pos = index;
-		}
-
-	}
-
-	if (hashtag_pos == npos)
-	{
-		return Token();
-	}
-
-	const StrBlob line = source.slice(hashtag_pos + 1, index);
-	string_char *escaped = new string_char[line.size + 1]{0};
-
-	size_t length = _escape(line, {escaped, line.size});
-
-	Token token = _parse_tk({escaped, length});
-	token.name = token.name.shifted(hashtag_pos + 1);
-	token.value = token.value.shifted(hashtag_pos + 1);
-
-	delete[] escaped;
-
-	return token;
-}
-
-CPreprocessor::Token CPreprocessor::_parse_tk(const StrBlob &line) {
-	constexpr auto _is_valid_macro_name = \
-		[](string_char value) { return value == '_' || isalnum(value); };
-
-	IndexRange name = {};
-	IndexRange value = {};
-
-	for (size_t i = 0; i < line.size; i++)
-	{
-		if (!name.valid())
-		{
-			if (_is_valid_macro_name(line[i]))
-			{
-				size_t count = string_tools::count(
-					line.data + i,
-					line.size - i,
-					_is_valid_macro_name
-				);
-
-				name = {i, i + count};
-
-				i += count;
-				i--;
-			}
-
-			continue;
-		}
-
-		if (!string_tools::is_whitespace(line[i]))
-		{
-			size_t trailing_ws = string_tools::rcount(
-				line.data + i,
-				line.size - i,
-				string_tools::is_whitespace
-			);
-
-			value = {i, line.size - trailing_ws};
-			break;
-		}
-	}
-
-	return Token(line.data, name, value);
-}
-
-size_t CPreprocessor::_escape(const StrBlob &source, const MutableStrBlob &destination) {
-	size_t offset = 0;
-	for (size_t i = 0; i < source.size; i++)
-	{
-		if (source[i] == '\n' && i < source.size - 1)
-		{
-			// next char + advance offset
-			i++;
-			offset += 2;
-
-			// skip escaped char
-			if (string_tools::is_whitespace(source[i]))
-			{
-				continue;
-			}
-
-			// revert back (only escaped whitespace in macros)
-			i--;
-			offset -= 2;
-		}
-
-		if (i - offset >= destination.size)
-		{
-			break;
-		}
-
-		destination[i - offset] = source[i];
-	}
-
-	destination[source.size - offset] = 0;
-
-	return source.size - offset;
-}
-
-CPreprocessor::Token::Token(const string_char *source, const IndexRange &_name, const IndexRange &_value)
-	: type{_get_tk_type(_name.to_blob(source))},
-	name{_name}, value{_value} {
-
-
+    LOG_ASSERT(source.current() == '\n');
+    source.skip(1);
+  }
 
 }
+
+CPreprocessor::Token CPreprocessor::get_next_tk(source_t source) {
+
+  bool non_preprocessor_line = false;
+  while (!source.depleted())
+  {
+    _try_skip_comment(source);
+
+    const char cur_chr = source.get();
+
+    if (non_preprocessor_line)
+    {
+      if (cur_chr == '\n')
+      {
+        non_preprocessor_line = false;
+      }
+
+      continue;
+    }
+
+    if (string_tools::is_whitespace(cur_chr))
+    {
+      continue;
+    }
+
+    if (cur_chr == '#')
+    {
+      // go back a character, so we are pointing at the '#'
+      source.seek(-1, SeekOrigin::Current);
+      return _parse_preprocessor_line(source);
+    }
+
+    non_preprocessor_line = true;
+  }
+
+  return Token();
+}
+
+string CPreprocessor::get_include_path(const std::string_view &value) {
+  constexpr auto is_opening = [](string_char value) {return value == '"' || value == '<';};
+  constexpr auto is_closing = [](string_char value) {return value == '"' || value == '>';};
+
+  // pos of the opening quote/bracket
+  size_t opening = CodeTokenizer::match_unescaped(
+    {value.data(), value.length()},
+    is_opening
+  );
+
+  if (opening == npos)
+  {
+    return "";
+  }
+
+  opening = opening + 1;
+
+  // pos of the closing quote/bracket
+  const size_t closing = CodeTokenizer::match_unescaped(
+    {value.data() + opening, value.length() - opening},
+    is_closing
+  ) + opening;
+
+  return string(value.data() + opening, closing - opening);
+}
+
+CPreprocessor::Token CPreprocessor::_parse_preprocessor_line(source_t source) {
+  LOG_ASSERT(source.current() == '#');
+  source.skip();
+
+  // skip white chars, not newlines + skip comments
+  _try_skip_to_usable(source);
+
+  // we expect a lowercase letter
+  if (!string_tools::is_lowercase(source.current()))
+  {
+    return Token();
+  }
+
+  const string name = _read_preprocessor_name(source);
+
+  // std::cout << "name: " << name << " - " << source.tell() << '\n';
+
+  _try_skip_to_usable(source);
+
+  // either we find a whitespace or a whitespace
+  if (source.depleted() || source.current() == '\n')
+  {
+    return Token(name, "");
+  }
+
+  const string value = _read_preprocessor_value(source);
+
+  // std::cout << "value: " << value << " - " << source.tell() << '\n';
+
+  return Token(name, value);
+}
+
+string CPreprocessor::_read_preprocessor_name(source_t source) {
+  string name = source.get_while(string_tools::is_lowercase);
+  return name;
+}
+
+string CPreprocessor::_read_preprocessor_value(source_t source) {
+  string value = _read_value(source);
+
+  // trim whitespace end
+  intptr_t index = intptr_t(value.size() - 1);
+  for (; index >= 0; index--)
+  {
+    if (!string_tools::is_whitespace(value[index]))
+    {
+      break;
+    }
+  }
+
+  value.resize(index + 1);
+
+  return value;
+}
+
+string CPreprocessor::_read_value(source_t source) {
+  string result = {0};
+
+  bool in_string = false;
+  while (!source.depleted())
+  {
+    const char cur = source.current();
+
+    
+    if (cur == '"')
+    {
+      in_string = !in_string; // toggle it
+    }
+    
+    if (in_string)
+    {
+      result.push_back(cur);
+      source.skip(1);
+      continue;
+    }
+
+    if (cur == '\n')
+    {
+      break;
+    }
+
+    _try_skip_comment(source);
+
+    if (source.depleted())
+    {
+      break;
+    }
+
+    result.push_back(source.get());
+  }
+
+  return result;
+}
+
+void CPreprocessor::_try_skip_comment(source_t source) {
+  if (source.available() < 2)
+  {
+    return;
+  }
+
+  if (source[0] != '/')
+  {
+    return;
+  }
+
+  // line comment
+  if (source[1] == '/')
+  {
+    source.skip_until([](char cur) { return cur == '\n'; });
+    return; // if not depleted, pointing to a newline
+  }
+
+  // block comment
+  if (source[1] == '*')
+  {
+    source.skip(2);
+
+    while (!source.depleted())
+    {
+      if (source[0] == '*' && source[1] == '/')
+      {
+        source.skip(2);
+        return;
+      }
+
+      source.skip(1);
+    }
+
+    // if not depleted, pointing to anything after '*/'
+    return;
+  }
+
+}
+
+void CPreprocessor::_try_skip_to_usable(source_t source) {
+  source.skip_while(whitespace_no_newlines);
+  _try_skip_comment(source);
+  source.skip_while(whitespace_no_newlines);
+}
+
+string CPreprocessor::_escape(const StrBlob &source) {
+  string result{};
+  result.reserve(source.length());
+
+  for (size_t i = 0; i < source.size; i++)
+  {
+    if (i + 1 < source.size && source[i] == '\\')
+    {
+      // skip escaped new lines
+      if (source[i + 1] == '\n')
+      {
+        continue;
+      }
+
+      result.push_back(source[++i]);
+      continue;
+    }
+
+    result.push_back(source[i]);
+  }
+
+
+  return result;
+}
+
+
+CPreprocessor::Token::Token(const string &name, const string &value)
+  : type{_get_tk_type({name.c_str(), name.length()})},
+  value{value} {}
--- src/code/CPreprocessor.hpp
+++ src/code/CPreprocessor.hpp
@@ -2,80 +2,90 @@
 #include "../base.hpp"
 #include "../Range.hpp"
 #include "../StringTools.hpp"
+#include "../io/CharSource.hpp"
+#include <string_view>
 
 struct CPreprocessor
 {
 
-	enum class Type : uint8_t
-	{
-		None,
-		Unknown,
+  enum class Type : uint8_t
+  {
+    None,
+    Unknown,
 
-		Include = 4,
+    Include = 4,
 
-		Define,
-		Undefine,
+    Define,
+    Undefine,
 
-		If,
-		Else,
-		Elif,
-		EndIf,
+    If,
+    Else,
+    Elif,
+    EndIf,
 
-		IfDefined,
-		IfNotDefined,
+    IfDefined,
+    IfNotDefined,
 
-		Error,
-		Warning,
+    Error,
+    Warning,
 
-		Pragma,
-	};
+    Pragma,
+  };
 
-	struct Token
-	{
-		inline Token() = default;
-		Token(const string_char *source, const IndexRange &name, const IndexRange &value);
+  struct Token
+  {
+    inline Token() = default;
+    Token(const string &name, const string &value);
 
-		inline bool is_valid() const {
-			return (int)type >= (int)Type::Include;
-		}
+    inline bool is_valid() const {
+      return (int)type >= (int)Type::Include;
+    }
 
-		Type type = Type::None;
-		IndexRange name;
-		IndexRange value; // or anything right of the statement
-		uint32_t line = 0;
-	};
+    Type type = Type::None;
+    string value; // or anything right of the statement
+    uint32_t line = 0;
+  };
 
-	static constexpr pair<const char *, Type> TypeNamePairs[] = {
-		{"include", Type::Include},
+  static constexpr pair<const char *, Type> TypeNamePairs[] = {
+    {"include", Type::Include},
 
-		{"define", Type::Define},
-		{"undef", Type::Undefine},
+    {"define", Type::Define},
+    {"undef", Type::Undefine},
 
-		{"if", Type::If},
-		{"else", Type::Else},
-		{"elif", Type::Elif},
-		{"endif", Type::EndIf},
+    {"if", Type::If},
+    {"else", Type::Else},
+    {"elif", Type::Elif},
+    {"endif", Type::EndIf},
 
-		{"ifdef", Type::IfDefined},
-		{"ifndef", Type::IfNotDefined},
+    {"ifdef", Type::IfDefined},
+    {"ifndef", Type::IfNotDefined},
 
-		{"error", Type::Error},
-		{"warning", Type::Warning},
+    {"error", Type::Error},
+    {"warning", Type::Warning},
 
-		{"pragma", Type::Pragma},
-	};
+    {"pragma", Type::Pragma},
+  };
+
+  using source_t = CharSource &;
 
 
-	static void gather_all_tks(const StrBlob &source, vector<Token> &output);
-	static Token get_next_tk(const StrBlob &source);
+  static void gather_all_tks(const StrBlob &input, vector<Token> &output);
+  static Token get_next_tk(source_t source);
 
-	static string get_include_path(const StrBlob &value);
+  static string get_include_path(const std::string_view &value);
 
-	static constexpr Type _get_tk_type(const StrBlob &name);
+private:
+  static constexpr Type _get_tk_type(const StrBlob &name);
 
-	static Token _read_tk_inline(const StrBlob &source);
-	static Token _parse_tk(const StrBlob &line);
+  static Token _parse_preprocessor_line(source_t source);
+  static string _read_preprocessor_name(source_t source);
+  static string _read_preprocessor_value(source_t source);
 
-	static size_t _escape(const StrBlob &source, const MutableStrBlob &destination);
+  static string _read_value(source_t source);
+
+  static void _try_skip_comment(source_t source);
+  static void _try_skip_to_usable(source_t source);
+
+  static string _escape(const StrBlob &source);
 
 };
