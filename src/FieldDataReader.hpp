c59db989eedc48f23866d117f924d322f8269241
BezarHere <botatodev@gmail.com>
2026-04-10 20:46:05 +0300

    field data reader refactoring + idk

--- src/FieldDataReader.hpp
+++ src/FieldDataReader.hpp
@@ -14,22 +14,27 @@ public:
       : _context{ context }, _data{ data } {}
 
   template <FieldVarType FType>
-  inline const FieldVar &try_get_value(const FieldVar::String &name,
-                                       const FieldVar &default_value = Default) const;
+  inline const FieldVar &try_get_value(
+      const FieldVar::String &name,
+      const FieldVar &default_value = Default) const;
 
   // tries to get an array with the name 'name'.
   // reports an error and returns null if the array doesn't exist or
   // if any element of the array isn't of the template type
   template <FieldVarType ElementFType>
-  inline const FieldVar &try_get_array(const FieldVar::String &name,
-                                       const FieldVar &default_value = DefaultArray) const;
+  inline const FieldVar &try_get_array(
+      const FieldVar::String &name,
+      const FieldVar &default_value = DefaultArray) const;
 
   // will throw an exception if the reader can't be created
-  // branching will fail if the value at 'name' isn't a dict (something else or does not exist)
-  // also, not specifying the new_context parameter will result in a reader with context equal to
-  // "<current context>::<sub-reader data name (i.e name parameter)>" (ex: foo.branch_reader("bar")
+  // branching will fail if the value at 'name' isn't a dict (something else or
+  // does not exist) also, not specifying the new_context parameter will result
+  // in a reader with context equal to
+  // "<current context>::<sub-reader data name (i.e name parameter)>" (ex:
+  // foo.branch_reader("bar")
   // -> foo::bar)
-  FieldDataReader branch_reader(const FieldVar::String &name, const string &new_context = "") const;
+  FieldDataReader branch_reader(const FieldVar::String &name,
+                                const string &new_context = "") const;
 
   inline const string &get_context() const noexcept { return _context; }
   inline const FieldVar::Dict &get_data() const noexcept { return _data; }
@@ -46,11 +51,13 @@ private:
 };
 
 template <FieldVarType FType>
-inline const FieldVar &FieldDataReader::try_get_value(const FieldVar::String &name,
-                                                      const FieldVar &default_value) const {
+inline const FieldVar &FieldDataReader::try_get_value(
+    const FieldVar::String &name,
+    const FieldVar &default_value) const {
   if (!_data.contains(name))
   {
-    _error("Expecting a value of type %s named '%s'", FieldVar::get_name_for_type(FType),
+    _error("Expecting a value of type %s named '%s', returning default",
+           FieldVar::get_name_for_type(FType),
            name.c_str());
     return default_value;
   }
@@ -59,8 +66,10 @@ inline const FieldVar &FieldDataReader::try_get_value(const FieldVar::String &na
 
   if (!var.is_convertible_to(FType))
   {
-    _error("Expecting type '%s' for '%s', got type '%s'", FieldVar::get_name_for_type(FType),
-           name.c_str(), FieldVar::get_name_for_type(var.get_type()));
+    _error("Expecting type '%s' for '%s', got type '%s'",
+           FieldVar::get_name_for_type(FType),
+           name.c_str(),
+           FieldVar::get_name_for_type(var.get_type()));
     return default_value;
   }
 
@@ -68,8 +77,9 @@ inline const FieldVar &FieldDataReader::try_get_value(const FieldVar::String &na
 }
 
 template <FieldVarType ElementFType>
-inline const FieldVar &FieldDataReader::try_get_array(const FieldVar::String &name,
-                                                      const FieldVar &default_value) const {
+inline const FieldVar &FieldDataReader::try_get_array(
+    const FieldVar::String &name,
+    const FieldVar &default_value) const {
   const FieldVar &array_var = try_get_value<FieldVarType::Array>(name);
 
   if (array_var.is_null())
@@ -82,9 +92,13 @@ inline const FieldVar &FieldDataReader::try_get_array(const FieldVar::String &na
     const auto &var = array_var.get_array()[i];
     if (!var.is_convertible_to(ElementFType))
     {
-      _error("The array '%s' should only contain %s types, but element at %llu is a/an %s type",
-             name.c_str(), FieldVar::get_name_for_type(ElementFType), i,
-             FieldVar::get_name_for_type(var.get_type()));
+      _error(
+          "The array '%s' should only contain %s types, but element at %llu is "
+          "a/an %s type",
+          name.c_str(),
+          FieldVar::get_name_for_type(ElementFType),
+          i,
+          FieldVar::get_name_for_type(var.get_type()));
       return default_value;
     }
   }
